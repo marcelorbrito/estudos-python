4b02b9fabca2aac390126669e899b54775694e42
Marcelo Rodrigues Brito <165288776+marcelorbrito@users.noreply.github.com>
2024-11-11 21:37:14 -0300

    Add files via upload
    aula sobre tipos de dados

---
+++ tipos_de_dados.py
@@ -0,0 +1,40 @@
+"""
+Tipos de dados:
+
+tipo String ou STR:
+
+São dados alfanuméricos que são armazenados como texto, muito ultilizados por exemplo para nomes, e devem ser referenciados entre aspas simples ou duplas, mas sempre sendo igual
+ao caractere de inicio
+"""
+#Ex:
+nome = 'Marcelo'
+print(nome)
+
+"""
+Tipo Int:
+São números INTEIROS sem casas decimais pode ser positivos ou negativos
+
+Tipo Float:
+São números com casa DECIMAIS negativos ou positivos pontos flutuante
+Obs: nas linguagens de programação não é usado virgula para representar as casas decimais.
+ao inves disso é usado ponto.
+"""
+#Ex:
+inteiro = 65 #int
+flutuante = 65.5 #Float
+
+print(type(inteiro))
+print(type(flutuante))
+
+""" 
+Tipo Booleano:
+
+Booleano são dados usados para testes lógicos os famosos True e False, verdadeiro e falso.
+
+"""
+#Ex:
+verdadeiro = True
+falso = False
+
+print(type(verdadeiro))
+print(type(falso))
