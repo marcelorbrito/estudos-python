b5277b26f675f7e63a46ec31cd036b04a852fc3e
Marcelo Rodrigues Brito <165288776+marcelorbrito@users.noreply.github.com>
2024-11-20 14:05:24 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,44 @@
+"""
+Variáveis assim como o nome diz são DADOS que atribuimos o valor a eles
+nomalmente são usadas para salvar na memória do computador para não precisar
+ficar repetindo a mesma coisa e tornar o código mais legivel
+"""
+#Ex:
+nome = "Marcelo Rodrigues Brito"
+print(nome)
+
+idade = 18
+print(idade)
+
+# podemos salvar operações matematicas também
+
+soma = 2 + 5
+print(soma)
+
+#frases e textos
+variavel = "Into é um variável, muito usado na programação"
+print(variavel)
+
+"""
+Se você pensar que em varios trechos do código, estamos usando um número 18 para representar a idade, e precisamos alterar a idade posteriormente
+ para 20, teriamos que mudar linha a linha como abaixo.
+"""
+print(18)
+print(18)
+print(18)
+print(18)
+print(18)
+print(18)
+print(18)
+
+"""
+Mas se usarmos uma variável, basta aletrarmos apenas na linha em que ela foi atribuida, e assim irá atomaticamente musar para o restante das linhas
+"""
+idade = 20
+print(idade)
+print(idade)
+print(idade)
+print(idade)
+print(idade)
+print(idade)
+print(idade)
